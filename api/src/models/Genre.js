cccb298171ddbf573a702248381610eacd18f075
Emilse <memilseo@gmail.com>
2024-03-12 17:37:01 -0300

    models

---
+++ api/src/models/Genre.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('Genre', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                len: [0, 15],
+            },
+        },
+    },
+    { timestamps: false });
+  };
