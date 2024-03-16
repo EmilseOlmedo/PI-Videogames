e24b83a5f8555e34c8fe640562a1b3358204ae55
Emilse <memilseo@gmail.com>
2024-03-16 13:08:53 -0300

    controller

--- api/src/controllers/gameByNameController.js
+++ api/src/controllers/gameByNameController.js
@@ -1,38 +1,17 @@
-// const axios = require ('axios');
-// const {Videogame, Genre} = require ('../db');
 const {getAllVideogames} = require('./allGamesController')
 
-//📍 UTILIZAR MÉTODO PARA EVITAR CARGA DE DUPLICADOS
 /*------------- OBTENGO VIDEOGAMES por NAME -------------*/
 const getVideogamesByName = async (name) =>{
     const allGames = await getAllVideogames();
-    // try {
-        const nameLower = name.toLowerCase()
-        const gameName = allGames.filter ((game)=>game.name.toLowerCase().include(nameLower))
-        if (gameName.length>15) { 
-            return gameName.slice(0, 15)}
-        // if (gameName.length>15) {}
-        // } else {
-            throw Error ('There are no games with that name');
-    // } catch (error) {
-    //     throw Error ('no funciona');
-    }
 
-// }
+    try {
+        const gameName = allGames.filter ((game)=>game.name.toLowerCase().includes(name.toLowerCase()))
+        if (gameName.length>0)
+            return gameName.slice(0, 2)
+            throw Error ('There are no games with that name');
+    } catch (error) {
+        throw Error ('no funciona');
+    }
+}
 
 module.exports = {getVideogamesByName}
-//controlo en mi BD
-// const dbGameName = await Videogame.findAll({
-//     where: query,
-//     include: {
-//         model: Genre,
-//         attributes: ['name'],
-//         through: {
-//             attributes: []
-//         }
-//     }
-// });
-// return dbGameName;
-
-// const apiGameName = await 
-
