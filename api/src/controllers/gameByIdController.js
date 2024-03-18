6260b583df6955235b2fbcffb15ae6cc2ae6bdec
Emilse <memilseo@gmail.com>
2024-03-18 00:19:30 -0300

    controller

--- api/src/controllers/gameByIdController.js
+++ api/src/controllers/gameByIdController.js
@@ -1,61 +1,66 @@
 const {apiVideogames} = require ('../Utils/utils');
 const {dbVideogames} = require('../Utils/utils');
 
-
-const getGamesById = async (id) => { //ME LLEGA UN ARRAY VACIO
+const getGamesById = async (id) => {
+    const source = isNaN(id)? 'db' : 'api';
     try {
-        if (isNaN(id)) {
-            const gameIdDb= await dbVideogames();
-            const gameDbFilter= await gameIdDb.filter(game=>game.id===id);
-            return (gameDbFilter)
-            // (gameIdApi) ? gameIdApi : res.send(`That ${id} does not exist`)
+        if(source==='api'){
+            return (await apiVideogames(id)).find(g=>g.id==id)
+        }else{
+            return (await dbVideogames(id)).find(g=>g.id==id)
+        }
+        } catch (error) {   
+            throw Error ('Error')
+        }
+}
+
+
+//     try {
+//         const allGames= await getAllVideogame();
+//         const gameFind= await allGames.find(game=>game.id==id)
+//         if (gameFind){
+//             console.log('acá')
+//             return gameFind
+//         } else { 
+//             console.log('o acáaaaaa')
+//             return ('No existe este id')
+
+//         }
+//     } catch (error) {
+//         throw Error ("Error: Falta el número de ID para buscar. ¡Intenta de nuevo!");
+//     }
+// }
+
+
+// const getGamesById = async (id) => { //ME LLEGA UN ARRAY VACIO
+    // try {
+    //     if (isNaN(id)) {
+    //         const gameIdDb= await dbVideogames().find(game=>game.id===id);
+            // const gameDbFilter= await gameIdDb.find(game=>game.id===idVideogame);
+            // return (gameIdDb)
+            // (gameDbFilter) ? gameDbFilter : res.send(`That ${id} does not exist`)
             
             // if (gameIdApifilter.length) 
             // console.log(gameIdApifilter)
             // return gameIdApifilter //: res.send(`That ${id} does not exist`)
-        } else {
-            const gameIdApi= await apiVideogames();
-            const gameIdApifilter = gameIdApi.filter((game)=> game.id===id)
-            if (gameIdApifilter.length>0){
-             return (gameIdApifilter)
-             } else {
-             throw new Error("No se encontraron juegos con ese ID en la API.")
+        // } else {
+        //     const gameIdApi= await apiVideogames().find((game)=> game.id===id)
+        //     console.log (gameIdApi)
+            // const gameIdApifilter = await gameIdApi.find((game)=> game.id===idVideogame)
             // console.log(gameIdApifilter)
-            // return (gameIdApifilter)
-             }
-        }
-    } catch (error) {
-        throw Error ("Error: Falta el número de ID para buscar. ¡Intenta de nuevo!");
-    }
-       
-    }
+            // if (gameIdApifilter.length>0){
+            //  return (gameIdApi)
+            // //  } else {
+            //  throw new Error("No se encontraron juegos con ese ID en la API.")
+            // // console.log(gameIdApifilter)
+            // // return (gameIdApifilter)
+            //  }
+    //     }
+    // } catch (error) {
+    //     throw Error ("Error: Falta el número de ID para buscar. ¡Intenta de nuevo!");
+    // }
+
+    // }
 
     
-// const getGamesById = async (id) => {
-//     try {
-//     if (isNaN(id)) {
-//         const gameIdDb= await dbVideogames();
-//         const gameDbFilter= gameIdDb.filter(game=>game.id===id);
-//         return (gameDbFilter)
-//         // (gameIdApi) ? gameIdApi : res.send(`That ${id} does not exist`)
-        
-//         // if (gameIdApifilter.length) 
-//         // console.log(gameIdApifilter)
-//         // return gameIdApifilter //: res.send(`That ${id} does not exist`)
-//     } else {
-//         const gameIdApi= await apiVideogames();
-//         const gameIdApifilter = gameIdApi.filter(game=> game.id===id)
-//         (gameIdApifilter.lenght>0)
-//         ? gameIdApifilter
-//         : (error) ('que está pasando')
-//         // console.log(gameIdApifilter)
-//         // return (gameIdApifilter)
-
-//     }
-// } catch (error) {
-//     throw Error ("Error: Falta el número de ID para buscar. ¡Intenta de nuevo!");
-// }
-   
-// }
-
 module.exports = {getGamesById}
--- api/src/routes/videogameRouter.js
+++ api/src/routes/videogameRouter.js
@@ -4,7 +4,7 @@ const {getVideogames, getVideogamesById} = require('../handlers/videogamesHandle
 const videogameRouter = Router();
 
 videogameRouter.get('/', getVideogames);
-videogameRouter.get('/:idVideogame', getVideogamesById);
+videogameRouter.get('/:id', getVideogamesById);
 // videogameRouter.post('/', createVideogame);
 
 module.exports= videogameRouter;
